1897092d716487a363211fac2d9c1d84544a445b
Fábio Costa <inesvdomingues@gmail.com>
2020-03-09 23:30:02 +0000

    exercise 8 bug corrected.

--- practicepython.org/ex8.py
+++ practicepython.org/ex8.py
@@ -7,18 +7,18 @@ if(player1 not in options):
  print("Invalid play!")
 else:
  player2=options[random.randint(0,2)]
+ print("Player 2: ", player2)
  
-print("Player 2: ", player2)
-#First check draws!
-if (player1==player2):
- print('Call it a draw!')
-else:
- #Find the winner:
- if player1 == "Rock" and player2 == "Scissors":
-   print("Player 1 won!")
- elif player1 == "Paper" and player2 == "Rock":
-   print("Player 1 won!")
- elif player1 == "Scissors" and player2 == "Paper":
-   print("Player 1 won!")
+ #First check draws!
+ if (player1==player2):
+  print('Call it a draw!')
  else:
+  #Find the winner:
+  if player1 == "Rock" and player2 == "Scissors":
+   print("Player 1 won!")
+  elif player1 == "Paper" and player2 == "Rock":
+   print("Player 1 won!")
+  elif player1 == "Scissors" and player2 == "Paper":
+   print("Player 1 won!")
+  else:
    print("Player 2 won!")
